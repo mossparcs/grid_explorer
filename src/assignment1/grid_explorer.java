d2031b280aadac1d69829b2782334a02c040252c
dgauci <frikxen@gmail.com>
2019-04-03 13:15:55 +0900

    Implemented code for updating information about a grid (red tile or object) and the robots position

--- src/assignment1/grid_explorer.java
+++ src/assignment1/grid_explorer.java
@@ -1,12 +1,32 @@
 package assignment1;
 
+import lejos.robotics.Color;
 import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3IRSensor;
 
 public class grid_explorer {
+	static final int OBJ_DIST = 10;
+	static final char OBJECT = 'O';
+	static final char RED_TILE = 'R';
+	static final int NORTH = 0, EAST = 90, SOUTH = 180, WEST = 270;
+	static final int TURN_DEG = 90;
+	
+	private static EV3ColorSensor color_sensor;
+	private static EV3IRSensor ir_sensor;
+	static char grid[][] = new char[6][4]; 
+	static int dir = NORTH;
+	static int x, y;
+	
 	public static void main(String[] args){
 		RegulatedMotor leftMotor = Motor.A;
 		RegulatedMotor rightMotor = Motor.D;
+		color_sensor = new EV3ColorSensor(SensorPort.S2);
+		ir_sensor = new EV3IRSensor(SensorPort.S1);
+
 		
 		one_forward(leftMotor, rightMotor);
 		stop(leftMotor, rightMotor);
@@ -26,6 +46,16 @@ public class grid_explorer {
 		try{
 			Thread.sleep(1900);
 		}catch(InterruptedException e){}
+		
+		// Update the robots location
+		if (dir == NORTH)
+			y += 1;
+		else if (dir == SOUTH)
+			y -= 1;
+		else if (dir == EAST)
+			x += 1;
+		else
+			x -= 1;
 	}
 	
 	
@@ -39,6 +69,8 @@ public class grid_explorer {
 		try{
 			Thread.sleep(1050);
 		}catch(InterruptedException e){}
+		
+		dir += TURN_DEG;
 	}
 	
 	//function to rotate to the right side, 90 degrees
@@ -52,11 +84,42 @@ public class grid_explorer {
 		try{
 			Thread.sleep(1050);
 		}catch(InterruptedException e){}
+		
+		dir -= TURN_DEG;
 	}
 	
 	//function to stop
 	public static void stop(RegulatedMotor leftMotor, RegulatedMotor rightMotor){
 		leftMotor.stop();
 		rightMotor.stop();
+	}
+	
+	// NOTE: color_sensor is a field. It can be passed as a parameter to be consistent with the movement methods
+	// Updates grid if it detects a red tile
+	public static void survey_color(){
+		if (color_sensor.getColorID() == Color.RED)
+			grid[x][y] = RED_TILE;
+	}
+	
+	// NOTE: ir_sensor is a field. It can be passed as a parameter to be consistent with the movement methods
+	// Updates grid if it detects an object
+	public static void survey_obj(){
+		SampleProvider distanceMode = ir_sensor.getDistanceMode();
+		float value[] = new float[distanceMode.sampleSize()];
+		distanceMode.fetchSample(value,  0);
+		
+		// Evaluate true only when the object is in an adjacent tile
+		if (value[0] <= OBJ_DIST)
+		{
+			// Update the correct adjacent tile
+			if (dir == NORTH)
+				grid[x][y + 1] = OBJECT;
+			else if (dir == SOUTH)
+				grid[x][y - 1] = OBJECT;
+			else if (dir == EAST)
+				grid[x + 1][y] = OBJECT;
+			else
+				grid[x - 1][y] = OBJECT;
+		}
 	}
 }
